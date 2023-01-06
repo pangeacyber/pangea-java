3f5ac2bf18072c01d43046723283d45eb7189030
David Wayman <david.wayman@pangea.cloud>
2023-01-06 15:18:03 -0800

    add doc strings for ip/url intel

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/intel/UrlIntelClient.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/intel/UrlIntelClient.java
@@ -48,18 +48,82 @@ public class UrlIntelClient extends Client{
         return resp;
     }
 
+    /**
+     * Look up a URL
+     * @pangea.description Retrieve a reputation score for a URL from a provider, including an optional detailed report.
+     * @param url The URL to be looked up
+     * @return UrlLookupResponse
+     * @throws PangeaException
+     * @throws PangeaAPIException
+     * @pangea.code
+     * {@code
+     * UrlLookupResponse response = client.lookup(
+     *     "http://113.235.101.11:54384");
+     * }
+     */
     public UrlLookupResponse lookup(String url) throws PangeaException, PangeaAPIException {
         return lookupPost(url, null, null, null);
     }
 
+    /**
+     * Look up a URL - provider
+     * @pangea.description Retrieve a reputation score for a URL from a provider, including an optional detailed report.
+     * @param url The URL to be looked up
+     * @param provider Use reputation data from this provider
+     * @return UrlLookupResponse
+     * @throws PangeaException
+     * @throws PangeaAPIException
+     * @pangea.code
+     * {@code
+     * UrlLookupResponse response = client.lookup(
+     *     "http://113.235.101.11:54384",
+     *     "crowdstrike");
+     * }
+     */
     public UrlLookupResponse lookup(String url, String provider) throws PangeaException, PangeaAPIException {
         return lookupPost(url, provider, null, null);
     }
 
+    /**
+     * Look up a URL - verbose, raw
+     * @pangea.description Retrieve a reputation score for a URL from a provider, including an optional detailed report.
+     * @param url The URL to be looked up
+     * @param verbose Echo the API parameters in the response
+     * @param raw Include raw data from this provider
+     * @return UrlLookupResponse
+     * @throws PangeaException
+     * @throws PangeaAPIException
+     * @pangea.code
+     * {@code
+     * UrlLookupResponse response = client.lookup(
+     *     "http://113.235.101.11:54384",
+     *     true,
+     *     true);
+     * }
+     */
     public UrlLookupResponse lookup(String url, boolean verbose, boolean raw) throws PangeaException, PangeaAPIException {
         return lookupPost(url, null, verbose, raw);
     }
 
+    /**
+     * Look up a URL - provider, verbose, raw
+     * @pangea.description Retrieve a reputation score for a URL from a provider, including an optional detailed report.
+     * @param url The URL to be looked up
+     * @param provider Use reputation data from this provider
+     * @param verbose Echo the API parameters in the response
+     * @param raw Include raw data from this provider
+     * @return UrlLookupResponse
+     * @throws PangeaException
+     * @throws PangeaAPIException
+     * @pangea.code
+     * {@code
+     * UrlLookupResponse response = client.lookup(
+     *     "http://113.235.101.11:54384",
+     *     "crowdstrike",
+     *     true,
+     *     true);
+     * }
+     */
     public UrlLookupResponse lookup(String url, String provider, boolean verbose, boolean raw) throws PangeaException, PangeaAPIException {
         return lookupPost(url, provider, verbose, raw);
     }

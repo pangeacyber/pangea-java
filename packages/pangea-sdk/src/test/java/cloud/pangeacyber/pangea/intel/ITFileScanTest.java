fbc25e37810ab892f1e35ce01a897ce754941c6e
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-08-08 17:30:12 -0300

    Update tests and examples

--- packages/pangea-sdk/src/test/java/cloud/pangeacyber/pangea/intel/ITFileScanTest.java
+++ packages/pangea-sdk/src/test/java/cloud/pangeacyber/pangea/intel/ITFileScanTest.java
@@ -1,6 +1,7 @@
 package cloud.pangeacyber.pangea.intel;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -14,41 +15,35 @@ import cloud.pangeacyber.pangea.intel.models.FileScanData;
 import cloud.pangeacyber.pangea.intel.requests.FileScanRequest;
 import cloud.pangeacyber.pangea.intel.responses.FileScanResponse;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ITFileScanTest {
 
-	final String EICAR = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*\n";
+	final String TESTFILE_PATH = "./src/test/java/cloud/pangeacyber/pangea/testdata/testfile.pdf";
+
 	FileScanClient client;
-	TestEnvironment environment = TestEnvironment.DEVELOP;
+	TestEnvironment environment = TestEnvironment.STAGING;
 
 	@Before
 	public void setUp() throws ConfigException {
 		client = new FileScanClient.Builder(Config.fromIntegrationEnvironment(environment)).build();
 	}
 
-	public File eicar() throws IOException {
-		File file = new File("file.exe");
-		try (FileOutputStream outputStream = new FileOutputStream(file)) {
-			outputStream.write(EICAR.getBytes());
-		}
-
-		return file;
-	}
-
 	@Test
 	public void testFileScan_Scan() throws PangeaException, PangeaException, PangeaAPIException, IOException {
-		File file = eicar();
-		FileScanResponse response = client.scan(new FileScanRequest.Builder().provider("crowdstrike").build(), file);
+		File file = new File(TESTFILE_PATH);
+		FileScanResponse response = client.scan(
+			new FileScanRequest.Builder().provider("crowdstrike").raw(true).verbose(false).build(),
+			file
+		);
 		assertTrue(response.isOk());
 
 		FileScanData data = response.getResult().getData();
-		assertEquals("malicious", data.getVerdict());
+		assertEquals("benign", data.getVerdict());
 		assertNull(response.getResult().getParameters());
-		assertNull(response.getResult().getRawData());
+		assertNotNull(response.getResult().getRawData());
 	}
 
 	@Test(expected = AcceptedRequestException.class)
@@ -58,7 +53,7 @@ public class ITFileScanTest {
 		cfg.setQueuedRetryEnabled(false);
 		client = new FileScanClient.Builder(cfg).build();
 
-		File file = eicar();
+		File file = new File(TESTFILE_PATH);
 		FileScanResponse response = client.scan(new FileScanRequest.Builder().provider("crowdstrike").build(), file);
 	}
 
@@ -70,25 +65,29 @@ public class ITFileScanTest {
 		client = new FileScanClient.Builder(cfg).build();
 
 		FileScanResponse response;
-		File file = eicar();
 		AcceptedRequestException exception = null;
 		try {
-			response = client.scan(new FileScanRequest.Builder().provider("crowdstrike").build(), file);
+			File file = new File(TESTFILE_PATH);
+			response =
+				client.scan(
+					new FileScanRequest.Builder().provider("crowdstrike").raw(true).verbose(false).build(),
+					file
+				);
 			assertTrue(false);
 		} catch (AcceptedRequestException e) {
 			exception = e;
 		}
 
-		// Sleep 60 seconds until result is (should) be ready
-		Thread.sleep(60 * 1000);
+		// Sleep 20 seconds until result is (should) be ready
+		Thread.sleep(20 * 1000);
 
 		// Poll result, this could raise another AcceptedRequestException if result is not ready
 		response = client.pollResult(exception.getRequestId(), FileScanResponse.class);
 		assertTrue(response.isOk());
 
 		FileScanData data = response.getResult().getData();
-		assertEquals("malicious", data.getVerdict());
+		assertEquals("benign", data.getVerdict());
 		assertNull(response.getResult().getParameters());
-		assertNull(response.getResult().getRawData());
+		assertNotNull(response.getResult().getRawData());
 	}
 }

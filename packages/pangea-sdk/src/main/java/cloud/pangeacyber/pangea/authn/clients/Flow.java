b1219fe665422380ff0564867538ac7c73671a1b
David Wayman <david.wayman@pangea.cloud>
2023-08-08 16:53:54 -0700

    Add doc strings for authn Flow, ClientToken

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/Flow.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/Flow.java
@@ -50,13 +50,43 @@ public class Flow extends AuthNBaseClient {
 		return reset;
 	}
 
-	// TODO: Doc
+	/**
+	 * Complete Sign-up/in
+	 * @pangea.description Complete a login or signup flow.
+	 * @pangea.operationId authn_post_v1_flow_complete
+	 * @param flowID An ID for a login or signup flow
+	 * @return FlowCompleteResponse
+	 * @throws PangeaException
+	 * @throws PangeaAPIException
+	 * @pangea.code
+	 * {@code
+	 * FlowCompleteResponse response = client.flow().complete("pfl_dxiqyuq7ndc5ycjwdgmguwuodizcaqhh");
+	 * }
+	 */
 	public FlowCompleteResponse complete(String flowID) throws PangeaException, PangeaAPIException {
 		FlowCompleteRequest request = new FlowCompleteRequest(flowID);
 		return post("/v1/flow/complete", request, FlowCompleteResponse.class);
 	}
 
-	// TODO: Doc
+	/**
+	 * Start a sign-up/in
+	 * @pangea.description Start a new signup or signin flow.
+	 * @pangea.operationId authn_post_v1_flow_start
+	 * @param request
+	 * @return FlowStartResponse
+	 * @throws PangeaException
+	 * @throws PangeaAPIException
+	 * @pangea.code
+	 * {@code
+	 * FlowStartResponse response = client.flow().start(
+	 * 	new FlowStartRequest.Builder()
+	 * 		.setCbURI("https://www.myserver.com/callback")
+	 * 		.setEmail("joe.user@email.com")
+	 * 		.setFlowType(FlowType.SIGNIN)
+	 * 		.setProvider(IDProvider.PASSWORD)
+	 * 		.build());
+	 * }
+	 */
 	public FlowStartResponse start(FlowStartRequest request) throws PangeaException, PangeaAPIException {
 		return post("/v1/flow/start", request, FlowStartResponse.class);
 	}

df1401ceecec01a432fc8a304473e69a724b8ff4
David Wayman <david.wayman@pangea.cloud>
2023-08-11 13:34:26 -0700

    add doc strings for flow reset and flow enroll mfa

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/FlowEnrollMFA.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/FlowEnrollMFA.java
@@ -30,13 +30,43 @@ public class FlowEnrollMFA extends AuthNBaseClient {
 		super(builder);
 	}
 
-	// TODO: Doc
+	/**
+	 * Complete MFA Enrollment
+	 * @pangea.description Complete MFA enrollment by verifying a trial MFA code.
+	 * @pangea.operationId authn_post_v1_flow_enroll_mfa_complete
+	 * @param request
+	 * @return FlowEnrollMFACompleteResponse
+	 * @throws PangeaException
+	 * @throws PangeaAPIException
+	 * @pangea.code
+	 * {@code
+	 * FlowEnrollMFACompleteResponse response = client.flow().enroll().mfa().complete(
+	 * 	new FlowVerifyMFACompleteRequest.Builder(
+	 * 		"pfl_dxiqyuq7ndc5ycjwdgmguwuodizcaqhh",
+	 * 		"391423").build());
+	 * }
+	 */
 	public FlowEnrollMFACompleteResponse complete(FlowVerifyMFACompleteRequest request)
 		throws PangeaException, PangeaAPIException {
 		return post("/v1/flow/enroll/mfa/complete", request, FlowEnrollMFACompleteResponse.class);
 	}
 
-	// TODO: Doc
+	/**
+	 * Start MFA Enrollment
+	 * @pangea.description Start the process of enrolling an MFA.
+	 * @pangea.operationId authn_post_v1_flow_enroll_mfa_start
+	 * @param request
+	 * @return FlowEnrollMFAStartResponse
+	 * @throws PangeaException
+	 * @throws PangeaAPIException
+	 * @pangea.code
+	 * {@code
+	 * FlowEnrollMFAStartResponse response = client.flow().enroll().mfa().start(
+	 * 	new FlowEnrollMFAStartRequest().Builder(
+	 * 		"pfl_dxiqyuq7ndc5ycjwdgmguwuodizcaqhh", 
+	 * 		MFAProvider.SMS_OTP).setPhone("1-808-555-0173").build());
+	 * }
+	 */
 	public FlowEnrollMFAStartResponse start(FlowEnrollMFAStartRequest request)
 		throws PangeaException, PangeaAPIException {
 		return post("/v1/flow/enroll/mfa/start", request, FlowEnrollMFAStartResponse.class);

aafc543927de6ec3d53d1158d877a48847d5da6b
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2023-08-11 15:33:29 -0300

    remove dupluicated class (#138)

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/requests/FlowVerifySocialRequest.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/requests/FlowVerifySocialRequest.java
@@ -1,61 +1,63 @@
 package cloud.pangeacyber.pangea.authn.requests;
-import cloud.pangeacyber.pangea.BaseRequest;
 
+import cloud.pangeacyber.pangea.BaseRequest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class FlowVerifySocialRequest extends BaseRequest{
-    @JsonProperty("flow_id")
-    private String flowID;
+public class FlowVerifySocialRequest extends BaseRequest {
 
-    @JsonInclude(Include.NON_NULL)
-    @JsonProperty("cb_state")
-    private String cbState;
+	@JsonProperty("flow_id")
+	private String flowID;
 
-    @JsonInclude(Include.NON_NULL)
-    @JsonProperty("cb_code")
-    private String cbCode;
+	@JsonInclude(Include.NON_NULL)
+	@JsonProperty("cb_state")
+	private String cbState;
 
-    private FlowVerifySocialRequest(Builder builder) {
-        this.flowID = builder.flowID;
-        this.cbState = builder.cbState;
-        this.cbCode = builder.cbCode;
-    }
+	@JsonInclude(Include.NON_NULL)
+	@JsonProperty("cb_code")
+	private String cbCode;
 
-    public String getFlowID() {
-        return flowID;
-    }
+	private FlowVerifySocialRequest(Builder builder) {
+		this.flowID = builder.flowID;
+		this.cbState = builder.cbState;
+		this.cbCode = builder.cbCode;
+	}
 
-    public String getCBState() {
-        return cbState;
-    }
+	public String getFlowID() {
+		return flowID;
+	}
 
-    public String getCBCode() {
-        return cbCode;
-    }
+	public String getCBState() {
+		return cbState;
+	}
 
-    public static class Builder {
-        private String flowID;
-        private String cbState;
-        private String cbCode;
+	public String getCBCode() {
+		return cbCode;
+	}
 
-        public Builder(String flowID) {
-            this.flowID = flowID;
-        }
+	public static class Builder {
 
-        public Builder withCBState(String cbState) {
-            this.cbState = cbState;
-            return this;
-        }
+		private String flowID;
+		private String cbState;
+		private String cbCode;
 
-        public Builder withCBCode(String cbCode) {
-            this.cbCode = cbCode;
-            return this;
-        }
+		public Builder(String flowID) {
+			this.flowID = flowID;
+		}
 
-        public FlowVerifySocialRequest build() {
-            return new FlowVerifySocialRequest(this);
-        }
-    }
+		public Builder withCBState(String cbState) {
+			this.cbState = cbState;
+			return this;
+		}
+
+		public Builder withCBCode(String cbCode) {
+			this.cbCode = cbCode;
+			return this;
+		}
+
+		public FlowVerifySocialRequest build() {
+			return new FlowVerifySocialRequest(this);
+		}
+	}
 }

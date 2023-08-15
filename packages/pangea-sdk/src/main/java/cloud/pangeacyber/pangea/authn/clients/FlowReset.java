dd054ffbdbdb567dbfb2910e4253b292c7175f75
David Wayman <david.wayman@pangea.cloud>
2023-08-15 09:47:49 -0700

    AuthN doc strings (#139)
    * add doc strings for flow reset and flow enroll mfa
    
    * Finishes adding AuthN doc strings
    
    * Add examples for user login password/social
    
    * fix embarbo tests
    
    ---------
    
    Co-authored-by: H. Andres Tournour <andres.tournour@pangea.cloud>

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/FlowReset.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/clients/FlowReset.java
@@ -12,6 +12,22 @@ public class FlowReset extends AuthNBaseClient {
 		super(builder);
 	}
 
+	/**
+	 * Password Reset
+	 * @pangea.description Reset password during sign-in.
+	 * @pangea.operationId authn_post_v1_flow_reset_password
+	 * @param request
+	 * @return FlowResetPasswordResponse
+	 * @throws PangeaException
+	 * @throws PangeaAPIException
+	 * @pangea.code
+	 * {@code
+	 * FlowResetPasswordResponse response = client.flow().reset().password(
+	 * 	new FlowResetPasswordRequest().Builder(
+	 * 		"pfl_dxiqyuq7ndc5ycjwdgmguwuodizcaqhh",
+	 * 		"My1s+Password").build());
+	 * }
+	 */
 	public FlowResetPasswordResponse password(FlowResetPasswordRequest request)
 		throws PangeaException, PangeaAPIException {
 		return post("/v1/flow/reset/password", request, FlowResetPasswordResponse.class);

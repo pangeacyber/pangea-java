6077630fe4dc1daf61210cdef74567e853abf0eb
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-09-14 10:41:08 -0300

    format

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/filters/FilterRange.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/filters/FilterRange.java
@@ -1,45 +1,44 @@
 package cloud.pangeacyber.pangea.filters;
 
-public class FilterRange<T> extends FilterEqual<T>{
+public class FilterRange<T> extends FilterEqual<T> {
 
-    public FilterRange(String name, Filter map){
-        super(name, map);
-    }
+	public FilterRange(String name, Filter map) {
+		super(name, map);
+	}
 
-    public void setLessThan(T value){
-        this.map.put(name + "__lt", value);
-    }
+	public void setLessThan(T value) {
+		this.map.put(name + "__lt", value);
+	}
 
-    @SuppressWarnings("unchecked")
-    public T getLessThan() {
-        return (T)this.map.get(name + "__lt");
-    }
+	@SuppressWarnings("unchecked")
+	public T getLessThan() {
+		return (T) this.map.get(name + "__lt");
+	}
 
-    public void setLessThanEqual(T value){
-        this.map.put(name + "__lte", value);
-    }
+	public void setLessThanEqual(T value) {
+		this.map.put(name + "__lte", value);
+	}
 
-    @SuppressWarnings("unchecked")
-    public T getLessThanEqual() {
-        return (T)this.map.get(name + "__lte");
-    }
+	@SuppressWarnings("unchecked")
+	public T getLessThanEqual() {
+		return (T) this.map.get(name + "__lte");
+	}
 
-    public void setGreaterThan(T value){
-        this.map.put(name + "__gt", value);
-    }
+	public void setGreaterThan(T value) {
+		this.map.put(name + "__gt", value);
+	}
 
-    @SuppressWarnings("unchecked")
-    public T getGreaterThan() {
-        return (T)this.map.get(name + "__gt");
-    }
+	@SuppressWarnings("unchecked")
+	public T getGreaterThan() {
+		return (T) this.map.get(name + "__gt");
+	}
 
-    public void setGreaterThanEqual(T value){
-        this.map.put(name + "__gte", value);
-    }
+	public void setGreaterThanEqual(T value) {
+		this.map.put(name + "__gte", value);
+	}
 
-    @SuppressWarnings("unchecked")
-    public T getGreaterThanEqual() {
-        return (T)this.map.get(name + "__gte");
-    }
-
+	@SuppressWarnings("unchecked")
+	public T getGreaterThanEqual() {
+		return (T) this.map.get(name + "__gte");
+	}
 }

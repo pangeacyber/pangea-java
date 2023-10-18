b223710b5640ad348e342cf05c5d69ec0e92cbce
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-10-18 14:48:05 -0300

    remove deprecated

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/authn/models/MFAProviders.java
+++
@@ -1,7 +0,0 @@
-package cloud.pangeacyber.pangea.authn.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.ArrayList;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class MFAProviders extends ArrayList<String> {}

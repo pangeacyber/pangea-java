f0ad2b6537c63c239b052f7cdb3a1e97099fe821
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-16 08:18:54 -0300

    SOS examples

---
+++ examples/store/put_transfer_method_post_url/src/main/java/cloud/pangeacyber/examples/App.java
@@ -0,0 +1,49 @@
+package cloud.pangeacyber.examples;
+
+import cloud.pangeacyber.pangea.store.StoreClient;
+import cloud.pangeacyber.pangea.store.requests.*;
+import cloud.pangeacyber.pangea.store.responses.*;
+import cloud.pangeacyber.pangea.exceptions.ConfigException;
+import cloud.pangeacyber.pangea.Config;
+import cloud.pangeacyber.pangea.TransferMethod;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) {
+        Config cfg = null;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+        String time = dtf.format(LocalDateTime.now());
+        String filepath = "./src/main/java/cloud/pangeacyber/examples/testfile.pdf";
+
+        try {
+            // Load service config from environment variables or create it with custom
+            // values
+            cfg = Config.fromEnvironment(StoreClient.serviceName);
+        } catch (ConfigException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        // Create Store client
+        StoreClient client = new StoreClient.Builder(cfg).build();
+
+        try {
+            System.out.println("Uploading file with post-url transfer method...");
+            File file = new File(filepath);
+            String name = time + "_file_post_url";
+            PutResponse respPut = client.put(
+                    new PutRequest.Builder().name(name).transferMethod(TransferMethod.POST_URL).build(),
+                    file);
+
+            System.out.printf("File upload success. Item ID: %s\n", respPut.getResult().getObject().getID());
+
+        } catch (Exception e) {
+            System.out.println("Failed to perform store request: " + e);
+            System.exit(1);
+        }
+
+    }
+}

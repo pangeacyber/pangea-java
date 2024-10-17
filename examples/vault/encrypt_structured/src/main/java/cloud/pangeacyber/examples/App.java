cb60e593dc0c439c214999d14f6a04e1f2642e5a
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-10-17 12:38:45 -0400

    fix(deps): update dependency cloud.pangea:pangea-sdk to v4

--- examples/vault/encrypt_structured/src/main/java/cloud/pangeacyber/examples/App.java
+++ examples/vault/encrypt_structured/src/main/java/cloud/pangeacyber/examples/App.java
@@ -12,44 +12,44 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) {
-        Config cfg = null;
-        try {
-            cfg = Config.fromEnvironment(VaultClient.serviceName);
-        } catch (ConfigException e) {
-            System.out.println(e);
-            System.exit(1);
-        }
+	public static void main(String[] args) {
+		Config cfg = null;
+		try {
+			cfg = Config.fromEnvironment(VaultClient.serviceName);
+		} catch (ConfigException e) {
+			System.out.println(e);
+			System.exit(1);
+		}
 
-        var client = new VaultClient.Builder(cfg).build();
+		var client = new VaultClient.Builder(cfg).build();
 
-        try {
-            // First create an encryption key, either from the Pangea Console or
-            // programmatically as below.
-            var dtf = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-            var time = dtf.format(LocalDateTime.now());
-            var generateResp = client.symmetricGenerate(
-                    new SymmetricGenerateRequest.Builder(
-                            SymmetricAlgorithm.AES256_CFB,
-                            KeyPurpose.ENCRYPTION,
-                            "Java example " + time).build());
-            var encryptionKeyId = generateResp.getResult().getId();
+		try {
+			// First create an encryption key, either from the Pangea Console or
+			// programmatically as below.
+			var dtf = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+			var time = dtf.format(LocalDateTime.now());
+			var generateResp = client.symmetricGenerate(
+					SymmetricGenerateRequest.builder()
+							.algorithm(SymmetricAlgorithm.AES256_CFB)
+							.purpose(KeyPurpose.ENCRYPTION)
+							.name("Java example " + time).build());
+			var encryptionKeyId = generateResp.getResult().getId();
 
-            // Structured data that we'll encrypt.
-            Map<String, Object> data = new LinkedHashMap<>();
-            data.put("foo", "bar");
-            data.put("some", "thing");
+			// Structured data that we'll encrypt.
+			Map<String, Object> data = new LinkedHashMap<>();
+			data.put("foo", "bar");
+			data.put("some", "thing");
 
-            // Encrypt
-            var encryptResponse = client.encryptStructured(
-                    new EncryptStructuredRequest.Builder<String, Object, Map<String, Object>>(
-                            encryptionKeyId,
-                            data,
-                            "$.foo").build());
-            System.out.println("Encrypted result: " + encryptResponse.getResult().getStructuredData());
-        } catch (Exception error) {
-            System.out.println("Error: " + error);
-            System.exit(1);
-        }
-    }
+			// Encrypt
+			var encryptResponse = client.encryptStructured(
+					new EncryptStructuredRequest.Builder<String, Object, Map<String, Object>>(
+							encryptionKeyId,
+							data,
+							"$.foo").build());
+			System.out.println("Encrypted result: " + encryptResponse.getResult().getStructuredData());
+		} catch (Exception error) {
+			System.out.println("Error: " + error);
+			System.exit(1);
+		}
+	}
 }

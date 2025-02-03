1adb01ed224bb6c94e4c1fcaab9cec35cd4dfa8a
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-02-03 11:52:30 -0500

    Support other LLM model inputs in AI Guard

--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/ai_guard/requests/TextGuardRequest.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/ai_guard/requests/TextGuardRequest.java
@@ -17,11 +17,32 @@ import lombok.extern.jackson.Jacksonized;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @SuperBuilder
 @Value
-public final class TextGuardRequest extends BaseRequest {
+public final class TextGuardRequest<TMessages> extends BaseRequest {
 
-	@NonNull
+	/**
+	 * Text to be scanned by AI Guard for PII, sensitive data, malicious
+	 * content, and other data types defined by the configuration. Supports
+	 * processing up to 10KB of text.
+	 */
 	String text;
 
+	/**
+	 * Structured data to be scanned by AI Guard for PII, sensitive data,
+	 * malicious content, and other data types defined by the configuration.
+	 * Supports processing up to 10KB of text.
+	 */
+	TMessages messages;
+
+	/**
+	 * Recipe key of a configuration of data types and settings defined in the
+	 * Pangea User Console. It specifies the rules that are to be applied to the
+	 * text, such as defang malicious URLs.
+	 */
 	String recipe;
+
+	/**
+	 * Setting this value to true will provide a detailed analysis of the text
+	 * data
+	 */
 	boolean debug;
 }
--- packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/ai_guard/responses/TextGuardResponse.java
+++ packages/pangea-sdk/src/main/java/cloud/pangeacyber/pangea/ai_guard/responses/TextGuardResponse.java
@@ -3,4 +3,4 @@ package cloud.pangeacyber.pangea.ai_guard.responses;
 import cloud.pangeacyber.pangea.Response;
 import cloud.pangeacyber.pangea.ai_guard.results.TextGuardResult;
 
-public final class TextGuardResponse extends Response<TextGuardResult> {}
+public final class TextGuardResponse<TMessages> extends Response<TextGuardResult<TMessages>> {}

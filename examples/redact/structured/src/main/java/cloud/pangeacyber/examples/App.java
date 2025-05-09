4ee1fe5f3c0ef78aef57fddaad5841ca28ef4980
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-05-09 13:37:35 -0400

    Add unredact and llm_request examples

--- examples/redact/structured/src/main/java/cloud/pangeacyber/examples/App.java
+++ examples/redact/structured/src/main/java/cloud/pangeacyber/examples/App.java
@@ -9,31 +9,31 @@ import java.util.HashMap;
 
 import cloud.pangeacyber.pangea.Config;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        Config cfg = null;
-        try {
-            cfg = Config.fromEnvironment(RedactClient.serviceName);
-        } catch (ConfigException e){
-            System.out.println(e);
-            System.exit(1);
-        }
+public class App {
+	public static void main(String[] args) {
+		Config cfg = null;
+		try {
+			cfg = Config.fromEnvironment(RedactClient.serviceName);
+		} catch (ConfigException e) {
+			System.out.println(e);
+			System.exit(1);
+			return;
+		}
 
-        RedactClient client = new RedactClient.Builder(cfg).build();
-        RedactStructuredResponse response = null;
-        HashMap<String, String> data = new HashMap<String, String>();
-        data.put("Name", "My name is Andres");
-        data.put("Contact", "My phone number is 123-456-7890");
-        try {
-            response = client.redactStructured(RedactStructuredRequest.builder().data(data).build());
-        } catch (Exception e){
-            System.out.println("Failed to perform redact: " + e);
-            System.exit(1);
-        }
+		final var client = new RedactClient.Builder(cfg).build();
+		RedactStructuredResponse response = null;
+		final var data = new HashMap<String, String>();
+		data.put("Name", "My name is Andres");
+		data.put("Contact", "My phone number is 123-456-7890");
+		try {
+			response = client.redactStructured(RedactStructuredRequest.builder().data(data).build());
+		} catch (Exception e) {
+			System.out.println("Failed to perform redact: " + e);
+			System.exit(1);
+			return;
+		}
 
-        System.out.println("Redact success");
-        System.out.println("Redacted data: " + response.getResult().getRedactedData());
-    }
+		System.out.println("Redact success");
+		System.out.println("Redacted data: " + response.getResult().getRedactedData());
+	}
 }

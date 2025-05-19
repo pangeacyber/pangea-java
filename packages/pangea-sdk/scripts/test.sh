c7e9d20300fe7ba702fc4a715ff0583beb1af84f
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-05-19 16:40:36 -0400

    Add mock integration tests

---
+++ packages/pangea-sdk/scripts/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd -- "$(dirname -- "$0")/.."
+
+npx start-server-and-test --expect 404 \
+  "npx @stoplight/prism-cli mock -d --json-schema-faker-fillProperties=false src/test/java/cloud/pangeacyber/pangea/testdata/ai-guard_openapi.json" \
+  4010 \
+  "mvn test -B -e -Dtest=cloud.pangeacyber.pangea.ai_guard.AIGuardTest"
--- packages/pangea-sdk/src/test/java/cloud/pangeacyber/pangea/ai_guard/ITAIGuardTest.java
+++ packages/pangea-sdk/src/test/java/cloud/pangeacyber/pangea/ai_guard/ITAIGuardTest.java
@@ -1,6 +1,5 @@
 package cloud.pangeacyber.pangea.ai_guard;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
